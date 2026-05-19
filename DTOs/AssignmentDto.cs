a1f6bb0a2528dea978f817a2e62851275eb77ec9
vertviger <vertviger@gmail.com>
2026-05-19 16:13:44 +0300

    First two DTOs

---
+++ DTOs/AssignmentDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.DTOs
+{
+    public class AssignmentDto
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = null!;
+        public DateTime Due_date = DateTime.MinValue;
+        public decimal Max_points { get; set; }
+        public string Published_status { get; set; } = null!;
+        public int SubmissionCount { get; set; }
+    }
+}
---
+++ DTOs/CourseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.DTOs
+{
+    public class CourseDto
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public int Credits { get; set; }
+        public List<AssignmentDto> Assignments = new List<AssignmentDto>();
+    }
+}
