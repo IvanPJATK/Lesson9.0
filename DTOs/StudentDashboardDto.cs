b273b4a0fc68d249703107f43637798706745aaa
vertviger <vertviger@gmail.com>
2026-05-20 13:23:31 +0300

    All required DTOs completed, might need fixes in the future

---
+++ DTOs/StudentDashboardDto.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.DTOs
+{
+    public class StudentDashboardDto
+    {
+        public int Id { get; set; }
+        public string IndexNumber { get; set; } = null!;
+        public string FullName { get; set; } = null!;
+        public bool IsActive { get; set; }
+        public List<StudentDashboardEnrollmentDto> Enrollments { get; set; } = new List<StudentDashboardEnrollmentDto>();
+        public List<SubmissionDto> Submissions { get; set; } = new List<SubmissionDto>();
+    }
+    public class StudentDashboardEnrollmentDto
+    {
+        public int Id { get; set; }
+        public string Status { get; set; } = null!;
+        public string CourseName { get; set; } = null!;
+    }
+}
---
+++ DTOs/SubmissionDto.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApplication1.DTOs
+{
+    public class SubmissionDto
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int AssignmentId { get; set; }
+        public string RepositoryURL { get; set; } = null!;
+        public string Status { get; set; } = null!;
+        public int? Score { get; set; }
+        public string? Feedback { get; set; }
+    }
+    public class CreateSubmissionDto
+    {
+        public int AssignmentId { get; set; }
+        public int StudentId { get; set; }
+        public string RepositoryURL { get; set; } = null!;
+    }
+
+    public class GradeSubmissionDto
+    {
+        public int Score { get; set; }
+        public string Feedback { get; set; } = null!;
+    }
+}
